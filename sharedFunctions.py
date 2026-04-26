f54e4bc40162be0e1586868f6d537f1e8013e94b
Loren Danaoui <ldanaoui@umich.edu>
2026-04-25 23:37:58 -0400

    plot fitting curves function

--- sharedFunctions.py
+++ sharedFunctions.py
@@ -35,3 +35,26 @@ def initializeWeights(inputSize, outputSize):
     W = np.random.uniform(low = -0.1, high = 0.1, size = (inputSize, outputSize)) 
     b = np.random.uniform(low = -0.1, high = 0.1, size = (1, outputSize)) 
     return W, b
+
+def plotFittingCurves(history, modelName):
+    plt.figure(figsize = (8, 5))
+    plt.plot(history['trainProxy'], label = 'Train Proxy Error')
+    plt.plot(history['testProxy'], label = 'Test Proxy Error')
+    plt.title(f'{modelName}: Proxy Error over Iterations')
+    plt.xlabel('Iterations')
+    plt.ylabel('Half MSE Proxy Error')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f"graphs/{modelName}_Fitting_Curve_Proxy.png")
+    plt.close()
+
+    plt.figure(figsize = (8, 5))
+    plt.plot(history['trainMisc'], label = 'Train Misclassification')
+    plt.plot(history['testMisc'], label = 'Test Misclassification')
+    plt.title(f'{modelName}: Misclassification Error over Iterations')
+    plt.xlabel('Iterations')
+    plt.ylabel('Error Rate')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f"graphs/{modelName}_Fitting_Curve_Misclassification.png")
+    plt.close()
