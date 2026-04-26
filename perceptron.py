678eac2facd697654d1bf2a843915a752ac8541f
gmarcus2006 <gmarcus@umich.edu>
2026-04-26 13:25:28 -0400

    Added fit function to perceptron file
    the functions trains the perceptron using gradient descent over a specified number of iterations

--- perceptron.py
+++ perceptron.py
@@ -44,3 +44,48 @@ class Perceptron:
         incorrect = np.sum(predictions != yLabels) 
         return incorrect / len(yLabels)
     
+    def fit(self, xTrain, yTrain, xTest = None, yTest = None, learningRate = 0.1, iterations = 100):
+        yTrainEncoded = oneHotEncode(yTrain, numClasses = 10)
+        
+        hasTestData = False
+        if xTest is not None and yTest is not None: 
+            yTestEncoded = oneHotEncode(yTest, numClasses = 10)
+            hasTestData = True
+            
+        history = {
+            'trainProxy': [], 'testProxy': [],
+            'trainMisc': [], 'testMisc': []
+        }
+        
+        bestMiscError = float('inf')
+        bestW = None
+        bestB = None
+
+        for i in range(iterations): 
+            Z, A = self.forward(xTrain)
+            
+            trainProxy = proxyError(yTrainEncoded, A) 
+            history['trainProxy'].append(trainProxy) 
+            
+            trainMisc = self.evaluateError(xTrain, yTrain) 
+            history['trainMisc'].append(trainMisc)
+            
+            if hasTestData: 
+                _, aTest = self.forward(xTest)
+                testProxy = proxyError(yTestEncoded, aTest)
+                testMisc = self.evaluateError(xTest, yTest)
+                history['testProxy'].append(testProxy)
+                history['testMisc'].append(testMisc)
+                
+            if trainMisc < bestMiscError:
+                bestMiscError = trainMisc
+                bestW = copy.deepcopy(self.W)
+                bestB = copy.deepcopy(self.b)
+                
+            dW, db = self.backward(xTrain, yTrainEncoded, A, Z) # back propagation to get the gradients
+            self.updateWeights(dW, db, learningRate)
+
+        self.W = bestW
+        self.b = bestB
+        
+        return history
